65079ddde2759e04f329598bb0847bc174fd61ff
Tanakorn666 <s6506021620059@email.kmutnb.ac.th>
2024-01-30 09:58:24 +0700

    U

---
+++ src/CRUDBookNoDB.js
@@ -0,0 +1,51 @@
+
+
+
+
+require("dotenv").config();
+const express = require('express');
+const app = express();
+
+//
+app.use(express.json());
+
+//
+let books = [
+    {
+        id: 1,
+        title: 'Book 1',
+        author: 'Author 1'
+    },
+    {
+        id: 2,
+        title: 'Book 2',
+        author: 'Author 2'
+    },
+    {
+        id: 3,
+        title: 'Book 3',
+        author: 'Author 3'  
+    }
+];
+
+//
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+//
+app.get('/book/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (!book) res.status(404).send('Book not found');
+    res.json(book);
+});
+
+//
+app.post('/books', (req, res) => {
+    const book = {
+        id: books.length + 1,
+        title: req.body.title,
+        author: req.body.author
+    };
+    books.push(book);
+})
