53bbdc2043868bc21a9a3989bf187dd389a59e29
Tanakorn666 <s6506021620059@email.kmutnb.ac.th>
2024-02-03 12:46:26 +0700

    U

--- Midterm/digitalClock.js
+++ Midterm/digitalClock.js
@@ -12,8 +12,13 @@ function digitalClock(seconds) {
 }
 
 function padZero(number) {
+    if (number == 24 && hours == 0) {
+        number = 0
+        hours ++
+    }
     return (number < 10) ? '0' + number : number.toString();
 }
+var hours = 0;
 
 console.log(digitalClock(5025));
 console.log(digitalClock(61201));
